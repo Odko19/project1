948a66a87b172dfde4a93c92725684d703727a6b
mstars_lab3_12 <mstars_lab3_12@mstars-lab3-12s-iMac.local>
2022-04-21 13:24:54 +0800

    ADD file

---
+++ Food delivery/js/app.js
@@ -0,0 +1,139 @@
+const food = [
+  {
+    name: "Зайрмаг",
+    price: "6800",
+    saleFood: "4700",
+    image: "./image/5.png",
+    discount: 30,
+    category: " Хямдралтай",
+  },
+  {
+    name: "Панкейк",
+    price: "6800",
+    saleFood: "4700",
+    image: "./image/4.png",
+    discount: 30,
+    category: " Хямдралтай",
+  },
+  {
+    name: "Өглөөний хоол",
+    price: "6800",
+    saleFood: "4700",
+    image: "./image/5.png",
+    category: " Хямдралтай",
+  },
+  {
+    name: "Банана сендвич",
+    price: "6800",
+    saleFood: "4700",
+    image: "./image/6.png",
+    category: " Хямдралтай",
+  },
+
+  {
+    name: "Салмон загас",
+    price: "6800",
+    saleFood: "4700",
+    image: "./image/4.png",
+    category: " Үндсэн хоол",
+  },
+  {
+    name: "Бөөрөнхий мах",
+    price: "6800",
+    saleFood: "4700",
+    image: "./image/5.png",
+    category: " Үндсэн хоол",
+  },
+  {
+    name: "Самрын нухаш",
+    price: "6800",
+    saleFood: "4700",
+    image: "./image/6.png",
+    category: " Үндсэн хоол",
+  },
+  {
+    name: "Чикен бургер",
+    price: "6800",
+    saleFood: "4700",
+    image: "./image/7.png",
+    category: " Үндсэн хоол",
+  },
+
+  {
+    name: "Детокс салад",
+    price: "6800",
+    saleFood: "4700",
+    image: "./image/8.png",
+    category: " Салад ба зууш",
+  },
+  {
+    name: "Кобб салад",
+    price: "6800",
+    saleFood: "4700",
+    image: "./image/9.png",
+    category: " Салад ба зууш",
+  },
+  {
+    name: "Авокадо салад",
+    price: "6800",
+    saleFood: "4700",
+    image: "./image/10.png",
+    category: " Салад ба зууш",
+  },
+  {
+    name: "Сендвич",
+    price: "6800",
+    saleFood: "4700",
+    image: "./image/11.png",
+    category: " Салад ба зууш",
+  },
+
+  {
+    name: "Донатс",
+    price: "6800",
+    saleFood: "4700",
+    image: "./image/12.png",
+    category: " Амттан",
+  },
+  {
+    name: "Орео дессерт",
+    price: "6800",
+    saleFood: "4700",
+    image: "./image/13.png",
+    category: " Амттан",
+  },
+  {
+    name: "Вафли",
+    price: "6800",
+    saleFood: "4700",
+    image: "./image/14.png",
+    category: " Амттан",
+  },
+  {
+    name: "Макарон",
+    price: "6800",
+    saleFood: "4700",
+    image: "./image/15.png",
+    category: " Амттан",
+  },
+];
+
+const cardd = document.querySelectorAll(".card");
+const cardTitle = document.querySelectorAll(".card-title ");
+const cardText = document.querySelectorAll(".card-text");
+const cardImage = document.querySelectorAll(".cardImage");
+
+for (let i = 0; i < food.length; i++) {
+  cardTitle[i].innerHTML += food[i].name;
+  cardText[i].innerHTML += `${food[i].price}  ${food[i].saleFood}`;
+  cardImage[
+    i
+  ].innerHTML += ` <img src="${food[i].image}" alt="" class="w-100">`;
+  //[i] => ingej bichij ogch b/gaa ni elementuud maain array-aar orj irj b.ga
+}
+
+let filter111 = food.filter((e) => {
+  let text = food.name;
+  return text;
+});
+console.log(filter111);
